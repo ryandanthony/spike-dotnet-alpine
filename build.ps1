52138507b3cc9fca4bfb73e8acd858cc75b4cce7
Ryan Anthony <ryandanthony@users.noreply.github.com>
2021-04-04 16:29:48 +0000

    spike done

---
+++ build.ps1
@@ -0,0 +1,8 @@
+#1/usr/bin/env pwsh
+Push-Location ./src/test
+    dotnet publish -c Release -f netcoreapp3.1 -r linux-musl-x64 -o out
+    docker build -t "test:latest" .
+
+Pop-Location
+docker run -p 8080:80 test:latest 
+curl -v http://localhost:8080/weatherforecast
